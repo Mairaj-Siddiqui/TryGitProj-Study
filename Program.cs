ec55cd7288ca335d2bc74085102001f8a5b10230
Mairaj-Siddiqui <148498576+Mairaj-Siddiqui@users.noreply.github.com>
2024-10-10 19:25:37 +0200

    More modifications

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
             {
             Console.WriteLine("Hello, World! C# PROGRAMMERS");
+            
+            Console.WriteLine("Make it double! C# PROGRAMMERS");
 
 
 
